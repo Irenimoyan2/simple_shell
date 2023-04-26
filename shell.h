677b40a27a676404d2b83141c5aad9ba0a48fc5c
1122Louis <louisdzidzor22@gmail.com>
2023-04-26 05:41:47 -0700

    My shell.h

--- shell.h
+++ shell.h
@@ -1,81 +1,33 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
-
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <signal.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
-#include <limits.h>
-#include <signal.h>
+#include <sys/stat.h>
+#define DELIM " ,!¡¿?\'\"\n\t"
+#define STDOUT STDOUT_FILENO
+#define STDIN STDIN_FILENO
+#define STDERR STDERR_FILENO
 
+extern char **environ;
+char **tokenizer(char *BUFF);
+char *check_path(char **Arg_str);
+void print_error(char *CMD);
+int built_in(char **Arg_str, int ct_output, char *row);
+int execute(char **Arg_str, int ct_output, char *row);
+int run(char **Arg_str, int ct_output);
+int compare_args(char **Arg_str, int ct_output);
+void handle_signal(int sig);
+char *read_row(void);
+char *get_env(void);
+int string_len_compare(char *string_1, char *string_2, int len);
+char *string_copy(char *dest, char *src);
+char *string_concatenate(char *dest, char *src);
+int string_compare(char *string_1, char *string_2);
+int string_length(char *a);
 
-/**
- * struct variables - variables
- * @av: command line arguments
- * @buffer: buffer of command
- * @env: environment variables
- * @count: count of commands entered
- * @argv: arguments at opening of shell
- * @status: exit status
- * @commands: double pointer to commands
- */
-typedef struct variables
-{
-	char **av;
-	char *buffer;
-	char **env;
-	size_t count;
-	char **argv;
-	int status;
-	char **commands;
-} vars_t;
-
-/**
- * struct builtins - struct for the builtin functions
- * @name: name of builtin command
- * @f: function for corresponding builtin
- */
-typedef struct builtins
-{
-	char *name;
-	void (*f)(vars_t *);
-} builtins_t;
-
-char **make_env(char **env);
-void free_env(char **env);
-
-ssize_t _puts(char *str);
-char *_strdup(char *strtodup);
-int _strcmpr(char *strcmp1, char *strcmp2);
-char *_strcat(char *strc1, char *strc2);
-unsigned int _strlen(char *str);
-
-char **tokenize(char *buffer, char *delimiter);
-char **_realloc(char **ptr, size_t *size);
-char *new_strtok(char *str, const char *delim);
-
-void (*check_for_builtins(vars_t *vars))(vars_t *vars);
-void new_exit(vars_t *vars);
-void _env(vars_t *vars);
-void new_setenv(vars_t *vars);
-void new_unsetenv(vars_t *vars);
-
-void add_key(vars_t *vars);
-char **find_key(char **env, char *key);
-char *add_value(char *key, char *value);
-int _atoi(char *str);
-
-void check_for_path(vars_t *vars);
-int path_execute(char *command, vars_t *vars);
-char *find_path(char **env);
-int execute_cwd(vars_t *vars);
-int check_for_dir(char *str);
-
-void print_error(vars_t *vars, char *msg);
-void _puts2(char *str);
-char *_uitoa(unsigned int count);
-
-#endif /* _SHELL_H_ */
+#endif
